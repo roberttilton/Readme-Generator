a3120160f70b549f5c1828e6bd47e339c725c6a4
Robert Tilton <robtilton2086@gmail.com>
2021-02-26 14:47:21 -0500

    got the license badges working, and cleaned up unnecessary file structure

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateREADME = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <p class="lead">My preferred hobby is ${answers.hobby}.</p>
-    <p class="lead">My favorite food at the moment is ${answers.food}!</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+  `# ${answers.title}
+  ## Description
+  ${answers.description}
+  ## Installation Instructions
+  ${answers.installation}
+  ## Usage Information
+  ${answers.usage}
+  ## License
+  ${answers.license}
+  ## Contribution Guidelines
+  ${answers.contribution}
+  ## Test Information
+  ${answers.test}`;
 
 inquirer
   .prompt([
@@ -54,7 +42,26 @@ inquirer
       type: 'rawlist',
       name: 'license',
       message: 'Which license would you like to use?',
-      choices: ['Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+      choices: [{
+        name: 'Apache License 2.0',
+        value: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+       },
+       {
+        name: 'GNU General Public License 3.0',
+        value: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+       },
+       {
+        name: 'MIT License',
+        value: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+       },
+       {
+        name: 'BSD 2-Clause "Simplified" License',
+        value: '[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+       },
+       {
+        name: 'Creative Commons Zero v1.0 Universal',
+        value: '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)'
+       }]
     },
     {
       type: 'input',
